dd46456efc22b17332846f217c60bf4e80b62baf
jreyescom <jreyescom84@gmail.com>
2024-09-06 20:36:23 -0500

    Latest Commit

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px 
 
+st.title("Anuncios de Autos en USA")
+st.write("Datos de anuncios de venta de autos en Estados Unidos")
+
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
         
